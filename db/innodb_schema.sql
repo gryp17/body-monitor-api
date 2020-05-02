0e87d83068c126c9cd11b3b41cfe09accf761fe7
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-05-02 12:14:50 +0300

    Added the database schema

--- db/innodb_schema.sql
+++ db/innodb_schema.sql
@@ -1,7 +1,26 @@
 
---
--- Table structure for table `user`
---
+CREATE TABLE `measurement` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `unit_id` int(11) DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `measurement_entry` (
+  `id` int(11) NOT NULL,
+  `measurement_id` int(11) DEFAULT NULL,
+  `value` float DEFAULT NULL,
+  `date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `measurement_unit` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `measurement_unit` (`id`, `name`) VALUES
+(1, 'kg'),
+(2, 'cm');
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -10,27 +29,25 @@ CREATE TABLE `user` (
   `registered` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `user`
---
+ALTER TABLE `measurement`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_measurement_user_id` (`user_id`),
+  ADD KEY `fk_measurement_unit_id` (`unit_id`);
+
+ALTER TABLE `measurement_entry`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_measurement_entry_id` (`measurement_id`);
+
+ALTER TABLE `measurement_unit`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `measurement`
+  ADD CONSTRAINT `fk_measurement_unit_id` FOREIGN KEY (`unit_id`) REFERENCES `measurement_unit` (`id`),
+  ADD CONSTRAINT `fk_measurement_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `measurement_entry`
+  ADD CONSTRAINT `fk_measurement_entry_id` FOREIGN KEY (`measurement_id`) REFERENCES `measurement` (`id`);
